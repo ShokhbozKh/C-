8dfd58e92fbc83611ff68eb11a94f92e1eca3b0e
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-19 00:25:59 +0500

    Add files via upload

---
+++ #1_nazarya.cpp
@@ -0,0 +1,198 @@
+/////// ctrl+ / bosilsa belgilangan joy izohi olinadi.
+//#1
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	float a, b, v, s;
+//	
+//	cout << "a="; cin >> a;
+//	cout << "b="; cin >> b;
+//	
+//	v = a * a * a;
+//	s = 6 * a * a;
+//	
+//	cout << "v=" << v << endl;
+//	cout << "s=" << s << endl;
+//	
+//	return 0;
+//}
+
+
+//#2
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	int a, b;
+//	float c, s;
+//	
+//	cout << "a="; cin >> a;
+//	cout << "b="; cin >> b;
+//	
+//	c = sqrt(a * a + b * b);
+//	s = a * b / 2;
+//	
+//	cout << "c=" << c << endl;
+//	cout << "s=" << s << endl;
+//	
+//	system("pause");
+//	return 0;
+//}
+
+
+//#3
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	float a, b, arif, geo;
+//	
+//	cout << "a="; cin >> a;
+//	cout << "b="; cin >> b;
+//	
+//	arif = ( a + b ) / 2;
+//	geo = sqrt(a * b);
+//	
+//	cout << "arif=" << arif << endl;
+//	cout << "geo=" << geo << endl;
+//	
+//	return 0;
+//}
+
+
+//#4
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	float h, t;
+//	const float g = 9.81;
+//	
+//	cout << "h="; cin >> h;
+//	
+//	t = sqrt( ( 2 * h ) / 2 );
+//	
+//	cout << "t=" << t << endl;
+//	
+//	return 0;
+//}
+
+
+//#5
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	float a, s;
+//	
+//	cout << "a="; cin >> a;
+//	
+//	s = sqrt ( 3 ) * pow ( a, 2) / 4;
+//	
+//	cout << "s=" << s << endl;
+//	
+//	return 0;
+//}
+
+
+//#6
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main() {
+//	
+//	float l, t;
+//	const float g = 9.81, pi = 3.14;
+//	
+//	cout << "l="; cin >> l;
+//	
+//	t = 2 * pi * sqrt (l / g);
+//	
+//	cout << "t=" << t << endl;
+//	
+//	return 0;
+//}
+
+
+//#7
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	float t1, t2, v1, v2, t, v;
+//	
+//	cout << "t1="; cin >> t1;
+//	cout << "t2="; cin >> t2;
+//	cout << "v1="; cin >> v1;
+//	cout << "v2="; cin >> v2;
+//	
+//	t = ( ( v1 + v2) * t2 + v1 * t1 ) / (2 * v1 + v2);
+//	v = v1 + v2;
+//	
+//	cout << "t=" << t << endl;
+//	cout << "v=" << v << endl;
+//	
+//	return 0;
+//}
+
+
+//#8
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	float r, r1, r2, r3;
+//	
+//	cout << "r1="; cin >> r1;
+//	cout << "r2="; cin >> r2;
+//	cout << "r3="; cin >> r3;
+//	
+//	r = (r1 * r2 * r3) / ( r1 * r2 + r1 * r3 + r2 * r3);
+//	
+//	cout << "r=" << r << endl;
+//	
+//	return 0;
+//}
+
+
+//#9
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	float x, y, natija;
+//	
+//	cout << "x="; cin >> x;
+//	cout << "y="; cin >> y;
+//	
+//	natija = ( fabs( x ) - fabs( y ) ) / ( 1 + fabs( x * y) );
+//	
+//	cout << "natija=" << natija << endl;
+//	
+//	return 0;
+//}
+
