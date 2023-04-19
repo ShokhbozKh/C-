995383e871937f7adca06d63922a617225427523
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-19 16:02:38 +0500

    Add files via upload

---
+++ #1_masalalar.cpp
@@ -0,0 +1,788 @@
+//#1
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	int a, p;
+//	
+//	cout<<"a="; cin>>a;
+//	
+//	p=4*a;
+//	
+//	cout<<"p="<<p;
+//	
+//	
+//	return 0;
+//}
+
+
+//#2
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	int a, s;
+//	cout<<"a="; cin>>a;
+//	
+//	s=a*a;
+//	cout<<"s="<<s;
+//	
+//	return 0;
+//}
+
+//#3
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	int a,b;
+//	float s, p;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	s=a*b;
+//	p=2*(a+b);
+//	
+//	cout<<"s="<<s<<endl;
+//	cout<<"p="<<p<<endl;
+//	
+//	return 0;
+//}
+
+//#4
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int d;
+//	float l;
+//	const float pi=3.14;
+//	
+//	cout<<"d="; cin>>d;
+//	
+//	l=pi*d;
+//	
+//	cout<<"l="<<l<<endl;
+//	
+//	return 0;
+//}
+
+//#5
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a;
+//	float v, s;
+//	
+//	cout<<"a="; cin>>a;
+//	
+//	v=pow(a,3);
+//	s=pow(a,2)*6;
+//	
+//	cout<<"v="<<v<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+
+//#6
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	
+//	int a, b, c, v, s;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	
+//	v=a*b*c;
+//	s=2*(a*b+b*c+a*c);
+//	
+//	cout<<"v="<<v<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+
+//#7
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float r, l, s;
+//	float const  pi=3.14;
+//	
+//	
+//	cout<<"r="; cin>>r;
+//	
+//	l=2*pi*r;
+//	s=pi*r*r;
+//	
+//	cout<<"l="<<l<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	
+//	return 0;
+//}
+
+//#8
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	cout<<"arfimetik: "<< (a+b) / 2 <<endl;
+//	
+//	return 0;
+//}
+
+//#9
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	cout<<"geometrik: "<<( sqrt(a*b) )<<endl;
+//	return 0;
+//}
+
+//#10
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b, sum, i, kv1, kv2;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	sum=a+b;
+//	i=a*b;
+//	kv1=a*a;
+//	kv2=b*b;
+//	
+//	cout<<"summa:"<<sum<<endl;
+//	cout<<"kupaytma:"<<i<<endl;
+//	cout<<"kv1="<<kv1<<endl;
+//	cout<<"kv2="<<kv2<<endl;
+//	
+//	return 0;
+//}
+
+//#11
+//#include <iostream>
+//#include <math.h>
+//
+// using namespace std;
+// int main(){
+//	int a, b, summ, i, abc1, abc2;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	summ=a+b;
+//	i=a*b;
+//	abc1=abs(a);
+//	abc2=abs(b);
+//	
+//	cout<<"sum="<<summ<<endl;
+//	cout<<"Kupaytma="<<i<<endl;
+//	cout<<"abs1="<<abc1<<endl;
+//	cout<<"abs2="<<abc2<<endl;    
+//	
+//	return 0;
+// }
+
+
+//#12
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//int main(){
+//	int a, b, c, p;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	c=sqrt(a*a+b*b);
+//	p=a+b+c;
+//	
+//	cout<<"p="<<p<<endl;
+//	cout<<"c="<<c<<endl;	
+//	return 0;
+//}
+
+//#13
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//int main(){
+//	float r1, r2, s3;
+//	float const pi=3.14;
+//	
+//	cout<<"r1="; cin>>r1;
+//	cout<<"r2="; cin>>r2;
+//	
+//	s3=pi*(r1-r2);
+//	
+//	cout<<"s3="<<s3<<endl;
+//	
+//	return 0;
+//}
+
+
+//#14
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float l, s, r;
+//	const float pi=3.14;
+//	
+//	cout<<"l="; cin>>l;
+//	
+//	r=l/2*pi;
+//	s=pi*r*r;
+//	
+//	cout<<"r="<<r<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+
+//#15
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float s, d, r;
+//	const float pi=3.14;
+//	
+//	cout<<"s="; cin>>s;
+//	
+//	r=sqrt(s/pi);
+//	d=2*r;
+//	
+//	cout<<"r="<<r<<endl;
+//	cout<<"d="<<d<<endl;
+//	
+//	return 0;
+//}
+
+//#16
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, b, l;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	l=abs(a-b);
+//	
+//	cout<<"l="<<l<<endl;
+//	
+//	return 0;
+//}
+
+////#17
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, b, c, ac, bc,summ;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	
+//	ac=abs(a-b);
+//	bc=abs(b-c);
+//	summ=ac+bc;
+//	
+//	cout<<"ac="<<ac<<endl;
+//	cout<<"bc="<<bc<<endl;
+//	cout<<"summ="<<summ<<endl;
+//	
+//	
+//	return 0;
+//}
+//
+////#18
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, b, c, ac, bc, i;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	
+//	ac=abs(a-b);
+//	bc=abs(b-c);
+//	i=ac*bc;
+//	
+//	cout<<"ac="<<ac<<endl;
+//	cout<<"bc="<<bc<<endl;
+//	cout<<"i="<<i<<endl;
+//	
+//	
+//	return 0;
+//}
+//
+//
+////#19
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x1, y1, x2, y2,p ,s;
+//	
+//	//	y2 ,x=0	//
+//	//			//
+//	//x1,y1		//x2,y2 y=0
+//	
+//	cout<<"x1="; cin>>x1;
+//	cout<<"y1="; cin>>y1;
+//	cout<<"x2="; cin>>x2;
+//	cout<<"y2="; cin>>y2;
+//	
+//	p=2*( (x2-x1) + (y2-y1));
+//	s=(x2-x1)*(y2-y1);
+//	
+//	cout<<"p="<<p<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+//
+////#20
+
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x1, y1, x2, y2, l;
+//	
+//	cout<<"x1="; cin>>x1;
+//	cout<<"y1="; cin>>y1;
+//	cout<<"x2="; cin>>x2;
+//	cout<<"y2="; cin>>y2;
+//	
+//	l=sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+//	
+//	cout<<"l="<<l<<endl;
+//	
+//	return 0;
+//}
+//
+////#21
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x1, y1, x2, y2, x3, y3, a, b, c, s, p;
+//	
+//	cout<<"x1="; cin>>x1;
+//	cout<<"y1="; cin>>y1;
+//	cout<<"x2="; cin>>x2;
+//	cout<<"y2="; cin>>y2;
+//	cout<<"x3="; cin>>x3;
+//	cout<<"y3="; cin>>y3;
+//	
+//	a=sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+//	b=sqrt(pow(x2-x3,2) + pow(y2-y3,2));
+//	c=sqrt(pow(x3-x1,2) + pow(y3-y1,2));
+//	p=a+b+c;
+//	s=sqrt(p*(p-a)*(p-b)*(p-c));
+//	
+//	cout<<"s="<<s<<endl;
+//	cout<<"p="<<p<<endl;
+//	
+//	return 0;
+//}
+//
+////#22
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b, c;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	c=a;
+//	a=b;
+//	b=c;
+//	
+//	cout<<"a="<<a<<endl;
+//	cout<<"b="<<b<<endl;
+//	
+//	return 0;
+//}
+//
+////#23
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b, c, d;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	d=a;
+//	a=b;
+//	b=c;
+//	c=d;
+//	
+//	cout<<"a="<<a<<endl;
+//	cout<<"b="<<b<<endl;
+//	cout<<"c="<<c<<endl;
+//	
+//	return 0;
+//}
+//
+////#24
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int a, b, c, d;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	d=a;
+//	a=c;
+//	c=b;
+//	b=d;
+//	
+//	cout<<"a="<<a<<endl;
+//	cout<<"b="<<b<<endl;
+//	cout<<"c="<<c<<endl;
+//	
+//	return 0;
+//}
+
+////#25
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x, y;
+//	cout<<"x="; cin>>x;
+//	
+//	y=3*pow(x,6)-6*x*x-7;
+//	
+//	cout<<"y="<<y<<endl;
+//	
+//	return 0;
+//}
+
+////#26
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x, y;
+//	cout<<"x="; cin>>x;
+//	
+//	y=4*pow(x-3,6)-7*pow(x-3,3)+2;
+//	
+//	cout<<"y="<<y<<endl;
+//	
+//	return 0;
+//}
+//
+////#27
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, a2, a4, a8;
+//	cout<<"a="; cin>>a;
+//	
+//	a2=pow(a,2);
+//	a4=pow(a,4);
+//	a8=pow(a,8);
+//	
+//	cout<<"a2="<<a2<<endl;
+//	cout<<"a4="<<a4<<endl;
+//	cout<<"a8="<<a8<<endl;
+//	
+//	return 0;
+//}
+//
+////#28
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, a2, a3, a5, a10, a15;
+//	cout<<"a="; cin>>a;
+//	
+//	a2=pow(a,2);
+//	a3=pow(a,3);
+//	a5=pow(a,5);
+//	a10=pow(a,10);
+//	a15=pow(a,15);
+//	
+//	cout<<"a2="<<a2<<endl;
+//	cout<<"a3="<<a3<<endl;
+//	cout<<"a5="<<a5<<endl;
+//	cout<<"a10="<<a10<<endl;
+//	cout<<"a15="<<a15<<endl;
+//	
+//	return 0;
+//}
+//
+//#29
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, rad;
+//	const float pi=3.14;
+//	
+//	cout<<"a="; cin>>a;
+//	
+//	rad=( a/180 )*pi;
+//	
+//	cout<<"rad:"<<rad<<endl;
+//	
+//	return 0;
+//}
+
+//
+////#30
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, rad;
+//	const float pi=3.14;
+//	
+//	cout<<"rad="; cin>>rad;
+//	
+//	a=rad*180/pi;
+//	
+//	cout<<"gradus:"<<a<<endl;
+//	
+//	return 0;
+//}
+//
+////#31
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	
+//	float Tf, Tc;
+//	cout<<"Tf="; cin>>Tf;
+//	
+//	Tc=(Tf-32)*5/9;
+//	
+//	cout<<"Tc="<<Tc<<endl;
+//	
+//	return 0;
+//}
+//
+////#32
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	
+//	float Tf, Tc;
+//	cout<<"Tc="; cin>>Tc;
+//	
+//	Tf=(Tc*9/5 + 32);
+//	
+//	cout<<"Tf="<<Tf<<endl;
+//	
+//	return 0;
+//}
+//
+////#33
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	
+//	float x, a, bir, y, a2;
+//	
+//	cout<<"x="; cin>>x;
+//	cout<<"a="; cin>>a;
+//	cout<<"y="; cin>>y;
+//	
+//	bir=a/x;
+//	a2=bir*y;
+//	
+//	cout<<"1 kg:"<<bir<<" sum"<<endl;
+//	cout<<y<<" kg: "<<a2<<endl;	
+//	
+//	return 0;
+//}
+//
+////#34
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x, a, y, b, n;
+//	
+//	cout<<"x="; cin>>x;
+//	cout<<"a="; cin>>a;
+//	cout<<"y="; cin>>y;
+//	cout<<"b="; cin>>b;
+//	
+//	n=(a/x)-(b/y);
+//	
+//	cout<<" shokolat "<<n <<" sumga qimmat."<<endl;
+//	
+//	return 0;
+//}
+//
+////#35
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	
+//	float v, u, t1, t2, s;
+//	
+//	cout<<"v="; cin>>v;
+//	cout<<"u="; cin>>u;
+//	cout<<"t1="; cin>>t1;
+//	cout<<"t2="; cin>>t2;
+//	
+//	s=(v+u)*t1 -(v-u)*t2;
+//	
+//	cout<<"s masofa yurgan:"<<s<<endl;
+//	
+//	return 0;
+//}
+
+
+//#36
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float v1, v2, s1, t, s2;
+//	
+//	cout<<"v1="; cin>>v1;
+//	cout<<"v2="; cin>>v2;
+//	cout<<"s1="; cin>>s2;
+//	cout<<"t="; cin>>t;
+//	
+//	s2=s1+t*(v1+v2);
+//	
+//	cout<<"s2="<<s2<<endl;
+//
+//	
+//	return 0;
+//}
+
+
+////#37
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float v1, v2, s1, t, s2;
+//	
+//	cout<<"v1="; cin>>v1;
+//	cout<<"v2="; cin>>v2;
+//	cout<<"s1="; cin>>s2;
+//	cout<<"t="; cin>>t;
+//	
+//	s2=s1-t*(v1+v2);
+//	
+//	cout<<"s2="<<s2<<endl;
+//	//manfiy javob chiqsa ular bir birini oldidan utib ketgan
+//	
+//	return 0;
+//}
+
+//#38
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float a, b, x;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	x=-b/a;
+//	
+//	cout<<"x="<<x<<endl;
+//	
+//	return 0;
+//}
+
+//#39
+//#include <iostream>
+//#include <math.h>
+//
+//using namespace std;
+//int main(){
+//	
+//	float a, b, c, x1, x2, d;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	
+//	d=b*b-4*a*c;
+//	x1=(-b + sqrt(d) / 2*a);
+//	x2=(-b - sqrt(d) / 2*a);
+//	
+//	cout<<"x1="<<x1<<endl;
+//	cout<<"x2="<<x2<<endl;
+//	
+//	return 0;
+//}
+
+//#40
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a1, a2, b1, b2, c1, c2, x, y, d;
+//	
+//	cout<<"a1:"; cin>>a1;
+//	cout<<"a2:"; cin>>a2;
+//	cout<<"b1:"; cin>>b1;
+//	cout<<"b2:"; cin>>b2;
+//	cout<<"c1:"; cin>>c1;
+//	cout<<"c2:"; cin>>c2;
+//	
+//	d=(a1*b2-a2*b1);
+//	y=(a1*c2-a2*c1) / d;
+//	x=(c1*b2-c2*b1) / d;
+//	
+//	c1=a1*x+b1*y;
+//	c2=a2*x+b2*y;
+//	
+//	cout<<"x="<<x<<endl;
+//	cout<<"y="<<y<<endl;
+//	cout<<"d="<<d<<endl;
+//	
+//	return 0;
+//}
+
+
+
+
+
+
