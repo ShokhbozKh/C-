2de1f5297848ebd2b068892d1a611fca3c6e2812
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-20 23:42:42 +0500

    Add files via upload

---
+++ #3_nazarya.cpp
@@ -0,0 +1,139 @@
+////#1
+//#include <iostream>
+//
+//using namespace std;
+//	int main(){
+
+//	int a=1, b=0, c=1, d=0;
+//	bool result;
+//	
+//	result=((a&&b)||(c&&d)) && (a||b);
+//	
+//	cout<<"natija:"<<result<<endl;
+//		
+//	return 0;
+//	}
+
+//#2
+//#include <iostream>
+//
+//using namespace std;
+//	int main(){
+//	int a, b;
+//	bool natija;
+//	
+//	cin>>a;
+//	cin>>b;
+//	
+//	natija=(a>0)||(b>0);
+//	
+//	cout<<"natija:"<<(natija);
+//	
+//	return 0;
+//	}
+
+//#3
+//#include <iostream>
+//
+//using namespace std;
+//	int main(){
+//	float a, b;
+//	bool r;
+
+//	cin>>a>>b;
+//	
+//	r=((a>0)&&!(b>0))||(!(a>0)&&(b>0));
+//	
+//	cout<<r;
+//	return 0;
+//	}
+
+//#4
+//#include <iostream>
+//
+//using namespace std;
+//	int main(){
+//	int n;
+//	bool r;
+
+//	cin>>n;
+//	
+//	r=(n%2==0);
+//	
+//	cout<<r;
+//	
+//	return 0;
+//	}
+
+//#5
+
+//#include <iostream>
+//
+//using namespace std;
+//	int main(){
+//	int a, b;
+//	bool r;
+
+//	cin>>a;
+//	cin>>b;
+//	
+//	r=((a%2==1)&&(b%2==1));
+//	cout<<r<<endl;
+//	
+//	return 0;
+//	}
+
+//#6
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a, b;
+//	bool r;
+//	
+//	cin>>a;
+//	cin>>b;
+//	
+//	r=((a%2==0)&&!(b%2==0))||((a%2==1)&&(b%2==0));
+//	
+//	cout<<r;
+//	
+//	return 0;
+//}
+
+//#7
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a, b, c;
+//	bool r;
+//	
+//	cin>>a;
+//	cin>>b;
+//	cin>>c;
+//	
+//	r=(c*c==a*a+b*b);
+//	
+//	cout<<r;
+//	
+//	return 0;
+//}
+
+//#8
+
+//#include <iostream>
+//
+//using namespace std;
+//int main(){
+//	float a, b, c;
+//	bool r;
+//	
+//	cin>>a>>b>>c;
+//	
+//	r=((a<b+c)&&(b<a+c)&&(c<a+b));
+//	
+//	cout<<r;
+//	
+//	return 0;
+//}
+
+
