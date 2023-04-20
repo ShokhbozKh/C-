d5f73a685a816f83c2415bed5af49e2230a196fe
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-20 10:32:26 +0500

    Add files via upload

---
+++ #2_nazarya.cpp
@@ -0,0 +1,149 @@
+//#1
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"n="; cin>>n;
+//	
+//	cout<<"yuzlar xonasidagi son:"<<n/100 <<endl;
+//	
+//	return 0;
+//}
+
+//#2
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	
+//	cout<<"n="; cin>>n;
+//	
+//	cout<<"n son oxirgi raqami:"<<n%10<<endl;
+//	cout<<"n sonni birinchi raqami:"<<n/10<<endl;
+//	cout<<" n soni raqamlari yig\"indisi:"<<n%10 + n/10<<endl;
+//	
+//	return 0;
+//}
+
+//#3
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float a, b, c, p, s;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	cout<<"c="; cin>>c;
+//	
+//	p=(a+b+c)/2;
+//	s=sqrt(p*(p-a)*(p-b)*(p-c));
+//	
+//	cout<<"p="<<p<<endl;
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+
+//#4
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float c, a, b, r;
+//	
+//	cout<<"c="; cin>>c;
+//	cout<<"a="; cin>>a;
+//	
+//	b= sqrt(c*c-a*a);
+//	r= (a+b+c) / a*b ;
+//	
+//	cout<<"b="<<b<<endl;
+//	cout<<"r="<<r<<endl;
+//	
+//	return 0;
+//}
+
+//#5
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float a, d, n, s;
+//	cout<<"a="; cin>>a;
+//	cout<<"d="; cin>>d;
+//	cout<<"n="; cin>>n;
+//	
+//	s=(2*a+d*(n-1))*n/2;
+//	
+//	cout<<"s="<<s<<endl;
+//	
+//	return 0;
+//}
+
+//#6
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	float x1, x2, y1, y2, d;
+//	
+//	cout<<"x1="; cin>>x1;
+//	cout<<"x2="; cin>>x2;
+//	cout<<"y1="; cin>>y1;
+//	cout<<"y2="; cin>>y2;
+//	
+//	d=sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+//	
+//	cout<<"d="<<d<<endl;
+//		
+//	return 0;
+//}
+
+//#7
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	float a,b;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	cout<<"butun qisim:"<<int(a/b)<<endl;
+//	
+//	return 0;
+//}
+
+//#8
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//int main(){
+//	float a, b;
+//	
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	a=a/b;
+//	
+//	cout<<"a/b verguldan keyingi 4 xona:"<<setprecision(4)<<fixed<<a<<endl;
+//	
+//	return 0;
+//}
+
+//#9
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a, b, c;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	
+//	c=a;
+//	a=b;
+//	b=c;
+//	
+//	cout<<"a="<<a<<endl;
+//	cout<<"b="<<b<<endl;
+//	
+//	return 0;
+//}
+
+
