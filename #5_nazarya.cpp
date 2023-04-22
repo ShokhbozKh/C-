d7d524394fcd74a97fc51f05acddbdb5cd25d78b
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-22 19:18:27 +0500

    Add files via upload

---
+++ #5_nazarya.cpp
@@ -0,0 +1,127 @@
+//#1
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cin>>n;
+//	switch(n)
+//	{
+//		case 1:cout<<"Yanvar";
+//		break;
+//		case 2:cout<<"Fevral";
+//		break;
+//		case 3:cout<<"Mart";
+//		break;
+//		case 4:cout<<"Aprel";
+//		break;
+//		case 5:cout<<"May";
+//		break;
+//		case 6:cout<<"Iyun";
+//		break;
+//		case 7:cout<<"Iyul";
+//		break;
+//		case 8:cout<<"Avgust";
+//		break;
+//		case 9:cout<<"Sentyabr";
+//		break;
+//		case 10:cout<<"Oktyabr";
+//		break;
+//		case 11:cout<<"Noyabr";
+//		break;
+//		case 12:cout<<"Dekabr";
+//		break;
+//		default: cout<<"bunday oy yuq";
+//	}
+//	return 0;
+//}
+
+//#2
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"qaysi oyni kiritamiz="; cin>>n;
+//	
+//	switch(n){
+//		case 1:
+//		case 2:
+//		case 12: cout<<"qish"; break;
+//		case 3:
+//		case 4:
+//		case 5: cout<<"Bahor"; break;
+//		case 6:
+//		case 7:
+//		case 8:cout<<"yoz"; break;
+//		case 9:
+//		case 10:
+//		case 11: cout<<"kuz"; break;
+//		
+//		default: cout<<"bunday oy yuq";
+//	}
+//	return 0;
+//}
+
+//#3
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"Fasl:  uchun son kirit:"; cin>>n;
+//	
+//	switch(n)
+//	{
+//		case 1:cout<<"Qish fasli:"<<endl;
+//			cout<<"1.Dekabr \n .Yanvar \n 3.fevral"<<endl;
+//		break;
+//		case 2: cout<<"Bahor fasli: \n 1.Mart \n 2.Aprel \n 3.May"<<endl;
+//		break;
+//		case 3: cout<<"yoz fasli: \n 1.Iyun \n 2.Iyul \n 3.Avgust"<<endl;
+//		break;
+//		case 4: cout<<"kuz fasli: \n 1.Sentyabr \n 2.Oktyabr \n 3.Noyabr"<<endl;
+//		break;
+//		
+//		default:cout<<"fasl raqami 1--4 gacha";
+//	}
+//}
+
+//#4
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main(){
+//	int n, bir, un; //0--100 gacha
+//
+//	cout<<"1 dan 99 gacha son kiriting:"; cin>>n;
+//	
+//	bir = (n % 10) ;
+//	un = (n / 10 );
+//	
+//	switch(un)
+//	{	
+//		case 1: cout<<"O'n "; break;
+//		case 2: cout<<"Yigirma "; break;
+//		case 3: cout<<"O'ttiz "; break;
+//		case 4: cout<<"Qirq "; break;
+//		case 5: cout<<"Ellik "; break;
+//		case 6: cout<<"Oltmish "; break;
+//		case 7: cout<<"Yetmish "; break;
+//		case 8: cout<<"Sakson "; break;
+//		case 9: cout<<"Tuqson "; break;
+//		
+//	}
+//	
+//	switch(bir)
+//	{
+//		case 1: cout<<"bir"; break;
+//		case 2: cout<<"ikki "; break;
+//		case 3: cout<<"uch "; break;
+//		case 4: cout<<"turt "; break;
+//		case 5: cout<<"besh "; break;
+//		case 6: cout<<"olti "; break;
+//		case 7: cout<<"yetti "; break;
+//		case 8: cout<<"sakkiz "; break;
+//		case 9: cout<<"tuqqiz "; break;
+//	}
+//	
+//	return 0;
+//}
