7914ca83fe312bda727620e921b3ae53d30a92da
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-23 20:59:08 +0500

    Add files via upload

---
+++ gotoshartsiz o'tish.cpp
@@ -0,0 +1,45 @@
+//#1
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n ;
+//	
+//	nishon:
+//		cout<<"1-5 son kirit:"; cin>>n;
+//		if(n>5)
+//		goto nishon;
+//	cout<<" tug\"ri kiritildi"<<endl;
+//	return 0;
+//}
+
+//#2
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n ;
+//	
+//	nishon:
+//		cout<<"1-7 son kirit:"; cin>>n;
+//		if( n>7)
+//		goto nishon;
+//	cout<<" tug\"ri kiritildi"<<endl;
+//	return 0;
+//}
+
+//#3
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a, b, i;
+//	cout<<"a="; cin>>a;
+//	cout<<"b="; cin>>b;
+//	nishon:
+//		a=i ;
+//		i++;
+//		cout<<i<<endl;
+//		
+//		if(i<b)
+//			goto nishon;
+//	
+//	return 0;
+//}
