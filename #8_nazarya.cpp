6f193dfbb052c1553eb098b215aaf6fde85c385a
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2023-04-28 09:54:30 +0500

    Add files via upload

---
+++ #8_nazarya.cpp
@@ -0,0 +1,45 @@
+
+
+//#1 
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a, b;
+//	cin>>a>>b;
+//	
+//	while(a!=b){
+//		if(a>b) a=a-b;
+//		else b-=a;
+//	}
+//	cout<<a<<endl;
+//	return 0;
+//}
+
+//#2
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n, m, butun=0;
+//	cin>>n>>m;
+//	while(n>m){
+//		n-=m;
+//		butun++;
+//	}
+//	cout<<"butun= "<<butun<<" \n qoldiq="<<n<<endl;
+//	return 0;
+//}
+
+//#3
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n, sum=0;
+//	cout<<"n="; cin>>n;
+//	while(n>0){
+//		sum+=n%10;
+//		n/=10;
+//	}
+//	cout<<sum<<endl;
+//	return 0;
+//}
+
